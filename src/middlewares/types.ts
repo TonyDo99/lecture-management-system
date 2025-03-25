d52d346a90e4472bf18ef456e3eb3a80709c51c5
phat.do <dotanphat20@gmail.com>
2025-03-25 10:26:50 +0700

    feat: setup authentication and user model info

---
+++ src/middlewares/types.ts
@@ -0,0 +1,10 @@
+// Define a custom interface for the authenticated user
+export interface IAuthenticatedUser {
+  id: string
+  email: string
+  role: string // Add other fields if needed
+}
+
+export interface IAuthRequest extends Request {
+  user: IAuthenticatedUser
+}
